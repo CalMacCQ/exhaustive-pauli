5b1a1d54abb67a7ed80463faec764cb4235dfd50
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2025-02-19 17:44:53 +0000

    get tket2 test example working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use tket2;
+
 fn main() {
-    println!("Hello, world!");
+    // Load a tket1 circuit.
+    let mut circ: tket2::Circuit =
+        tket2::serialize::load_tk1_json_file("./test_files/barenco_tof_5.json").unwrap();
+
+    assert_eq!(circ.qubit_count(), 9);
+    assert_eq!(circ.num_operations(), 170);
+
+    // Traverse the circuit and print the gates.
+    //for command in circ.commands() {
+    //    println!("{:?}", command.optype());
+    //}
+
+    // Render the circuit as a mermaid diagram.
+    println!("{}", circ.mermaid_string());
+    println!(
+        "{}",
+        "======================================================================="
+    );
+    // Optimise the circuit.
+    tket2::passes::apply_greedy_commutation(&mut circ).unwrap();
+
+    // Render optimised circuit.
+    println!("{}", circ.mermaid_string());
 }
