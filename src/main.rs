017864cd77e9a0cad9e440efef8e2bedffd921d3
Callum Macpherson <93673602+CalMacCQ@users.noreply.github.com>
2025-02-20 18:55:57 +0000

    feat: rust subgraph finding and python circuit generation (#1)
    * set up project
    
    * get tket2 test example working
    
    * BFS implementation
    
    * add some test circuits
    
    * python circuit generation script
    
    * sibling subgraph extraction
    
    * circuit reloading
    
    * get subcircuit extraction working
    
    * json formatting
    
    * start adding rewrite rules
    
    * set up uv python project
    
    * update main.py

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use tket2::{
+    self,
+    hugr::{hugr::views::SiblingSubgraph, ops::NamedOp, HugrView, Node},
+};
+
+fn subgraph_from_source_node(circuit: &tket2::Circuit, source_node: Node) -> SiblingSubgraph {
+    // Traverse the Circuit with BFS to find the subcircuit
+    // in the causal cone of the source node
+
+    let mut visited_nodes: Vec<Node> = Vec::new();
+    let mut nodes_to_visit = vec![source_node];
+
+    visited_nodes.push(source_node);
+
+    while nodes_to_visit.len() > 0 {
+        let current_node = nodes_to_visit.pop().unwrap();
+        for neighbour in circuit.hugr().output_neighbours(current_node) {
+            if !visited_nodes.contains(&neighbour) {
+                visited_nodes.push(neighbour);
+                nodes_to_visit.push(neighbour);
+            }
+        }
+    }
+    SiblingSubgraph::try_from_nodes(visited_nodes, circuit.hugr()).unwrap()
+}
+
+fn main() {
+    let circ: tket2::Circuit =
+        tket2::serialize::load_tk1_json_file("./test_files/goto.json").unwrap();
+
+    println!("{}", circ.mermaid_string());
+
+    let pauli_z_cmds: Vec<_> = circ
+        .commands()
+        .filter(|cmd| cmd.optype().name() == "tket2.quantum.Z")
+        .collect();
+
+    let first_pauli = &pauli_z_cmds[0];
+
+    let sibgraph = subgraph_from_source_node(&circ, first_pauli.node());
+
+    let extracted = sibgraph.extract_subgraph(circ.hugr(), "my_HUGR_subgraph");
+
+    let subcircuit = tket2::Circuit::try_new(&extracted, extracted.root()).unwrap();
+    println!("{:?}", subcircuit);
+}
