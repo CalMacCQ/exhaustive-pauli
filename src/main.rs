35640b0511c6e128e365c035ac9b1522512215b2
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2025-02-20 16:51:52 +0000

    BFS implementation

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,29 @@
-use tket2;
+use tket2::{
+    self,
+    hugr::{hugr::views::SiblingSubgraph, HugrView, Node},
+};
+
+fn subgraph_from_source_node(circuit: tket2::Circuit, source_node: Node) -> SiblingSubgraph {
+    let mut visited_nodes: Vec<Node> = Vec::new();
+    let mut nodes_to_visit = vec![source_node];
+
+    visited_nodes.push(source_node);
+
+    while nodes_to_visit.len() > 0 {
+        let current_node = nodes_to_visit.pop().unwrap();
+        for neighbour in circuit.hugr().output_neighbours(current_node) {
+            if !visited_nodes.contains(&neighbour) {
+                visited_nodes.push(neighbour);
+                nodes_to_visit.push(neighbour);
+            }
+        }
+    }
+    SiblingSubgraph::try_from_nodes(visited_nodes, circuit.hugr()).unwrap()
+}
 
 fn main() {
-    // Load a tket1 circuit.
-    let mut circ: tket2::Circuit =
-        tket2::serialize::load_tk1_json_file("./test_files/barenco_tof_5.json").unwrap();
-
-    assert_eq!(circ.qubit_count(), 9);
-    assert_eq!(circ.num_operations(), 170);
-
-    // Traverse the circuit and print the gates.
-    //for command in circ.commands() {
-    //    println!("{:?}", command.optype());
-    //}
-
-    // Render the circuit as a mermaid diagram.
-    println!("{}", circ.mermaid_string());
-    println!(
-        "{}",
-        "======================================================================="
-    );
-    // Optimise the circuit.
-    tket2::passes::apply_greedy_commutation(&mut circ).unwrap();
-
-    // Render optimised circuit.
-    println!("{}", circ.mermaid_string());
+    let circ: tket2::Circuit =
+        tket2::serialize::load_tk1_json_file("./test_files/goto.json").unwrap();
+    //let extarcted =  subgraph_from_source_node(circ, start_node);
+    //let subcircuit = Circuit::try_new(extracted, extracted.root())
 }
